051dbf7e235a0bbbee04225e1b5d960e9bc32b41
Daniel Schuette <d.schuette@online.de>
2018-08-12 00:29:48 -0400

    add skeleton of parse.go

---
+++ parse.go
@@ -0,0 +1,12 @@
+package cloningprimer
+
+type restrictEnzyme struct {
+	// TODO: implement
+}
+
+// ParseEnyzmesFromFile parses enzyme data (identifiers, recognition sequences, etc.) from
+// a *.re file (see the example in ./enzymes.re) and returns a map with enzyme names as
+// keys and `restricEnzyme' structs as values
+func ParseEnyzmesFromFile(file string) map[string]string {
+
+}
