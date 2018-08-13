07089f0db8995a20fd1326c253b540ac8643150c
Daniel Schuette <d.schuette@online.de>
2018-08-12 23:44:58 -0400

    add webapp template

--- app/webapp.go
+++ app/webapp.go
@@ -1,3 +1,22 @@
 package main
 
-// TODO: implement web interface
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.New("index.html").ParseFiles("app/templates/index.html"))
+	err := t.Execute(w, nil)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v\n", err)
+		log.Fatal(err)
+	}
+}
