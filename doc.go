63a5579cb32b64482af8797ad8403fa282e922e6
Daniel Schuette <d.schuette@online.de>
2018-08-13 00:03:12 -0400

    add example to doc.go

--- doc.go
+++ doc.go
@@ -35,7 +35,30 @@
 //
 // Code Example:
 //
-// currently in ./cmd/main.go
+/*
+	// find forward primer with EcoRI restriction site and 5 random nucleotides
+	input := "ATGCAAAAACGGGCGATTTATCCGGGTACTTTCGATCCCATTACCAATGGTCATATCGATATCGTGACGCGCGCCACGCAGATGTTCGATCACGTTATTCTGGCGATTGCCGCCAGCCCCAGTAAAAAACCGATGTTTACCCTGGAAGAGCGTGTGGCACTGGCACAGCAGGCAACCGCGCATCTGGGGAACGTGGAAGTGGTCGGGTTTAGTGATTTAATGGCGAACTTCGCCCGTAATCAACACGCTACGGTGCTGATTCGTGGCCTGCGTGCGGTGGCAGATTTTGAATATGAAATGCAGCTGGCGCATATGAATCGCCACTTAATGCCGGAACTGGAAAGTGTGTTTCTGATGCCGTCGAAAGAGTGGTCGTTTATCTCTTCATCGTTGGTGAAAGAGGTGGCGCGCCATCAGGGCGATGTCACCCATTTCCTGCCGGAGAATGTCCATCAGGCGCTGATGGCGAAGTTAGCGTAG"
+	forward, err := cloningprimer.FindForward(input, "GAATTC", 1, 18, 4, false)
+	if err != nil {
+		log.Fatalf("error finding forward primer: %s\n", err)
+	}
+	if forward == "" {
+		log.Fatalf("no forward primer found\n")
+	}
+
+	// find reverse primer with BamHI restriction site and 3 random nucleotides
+	reverse, err := cloningprimer.FindReverse(input, "GGATCC", 1, 20, 3, true)
+	if err != nil {
+		log.Fatalf("error finding reverse primer: %s\n", err)
+	}
+	if reverse == "" {
+		log.Fatalf("no reverse primer found\n")
+	}
+
+	// print results
+	fmt.Printf("input: %s\nforward primer: %s\nreverse primer: %s\n", input, forward, reverse)
+*/
+//
 //
 // TODO: move example to ./doc.go
 //
