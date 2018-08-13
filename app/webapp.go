81dd5ff959180f2e47609dcbeddee4ceeb6d8119
Daniel Schuette <d.schuette@online.de>
2018-08-12 23:57:19 -0400

    add dummy code to app/templates/index.html

--- app/webapp.go
+++ app/webapp.go
@@ -12,9 +12,13 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+type Example struct {
+	Result string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.New("index.html").ParseFiles("app/templates/index.html"))
-	err := t.Execute(w, nil)
+	err := t.Execute(w, Example{Result: "hello gopher"})
 	if err != nil {
 		fmt.Fprintf(w, "error: %v\n", err)
 		log.Fatal(err)
