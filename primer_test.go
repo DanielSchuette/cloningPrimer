113f0a486e1a27a0056614784fac322807aa02f2
Daniel Schuette <d.schuette@online.de>
2018-08-12 21:15:52 -0400

    add preliminary test cases for func FindReverse

--- primer_test.go
+++ primer_test.go
@@ -12,12 +12,12 @@ type testCasePrimer struct {
 }
 
 type inputForPrimer struct {
-	seq        string
-	restrict   string
-	seqStart   int
-	length     int
-	random     int
-	startCodon bool
+	seq      string
+	restrict string
+	seqStart int
+	length   int
+	random   int
+	addCodon bool
 }
 
 func TestFindForward(t *testing.T) {
@@ -26,24 +26,24 @@ func TestFindForward(t *testing.T) {
 		// test `seq' with non-nucleotide letters
 		{
 			in: inputForPrimer{
-				seq:        "QVDASTGASD", /* first invalid letter should result in an error */
-				restrict:   "GAATTC",
-				seqStart:   3,
-				length:     3,
-				random:     4,
-				startCodon: true,
+				seq:      "QVDASTGASD", /* first invalid letter should result in an error */
+				restrict: "GAATTC",
+				seqStart: 3,
+				length:   3,
+				random:   4,
+				addCodon: true,
 			},
 			want: "",
 			err:  errors.New("invalid input Q at position 1, expected sequence of lower or upper case A,T,C,G"),
 		},
 		{
 			in: inputForPrimer{
-				seq:        "ATGCCGVDASTGASD", /* first invalid letter should result in an error */
-				restrict:   "GAATTC",
-				seqStart:   3,
-				length:     3,
-				random:     4,
-				startCodon: true,
+				seq:      "ATGCCGVDASTGASD", /* first invalid letter should result in an error */
+				restrict: "GAATTC",
+				seqStart: 3,
+				length:   3,
+				random:   4,
+				addCodon: true,
 			},
 			want: "",
 			err:  errors.New("invalid input V at position 7, expected sequence of lower or upper case A,T,C,G"),
@@ -51,12 +51,12 @@ func TestFindForward(t *testing.T) {
 		// test `seq' that is exactly of length (`length' + `seqStart' - 1)
 		{
 			in: inputForPrimer{
-				seq:        "ATGCCGTCGCATTCTG",
-				restrict:   "GAATTC",
-				seqStart:   1,
-				length:     16,
-				random:     4,
-				startCodon: true,
+				seq:      "ATGCCGTCGCATTCTG",
+				restrict: "GAATTC",
+				seqStart: 1,
+				length:   16,
+				random:   4,
+				addCodon: true,
 			},
 			want: "AGCTGAATTCATGCCGTCGCATTCTG", /* the expected output is the entire sequence plus overhang */
 			err:  nil,
@@ -64,12 +64,12 @@ func TestFindForward(t *testing.T) {
 		// test `seq' that is shorter than (`length' + `seqStart' - 1)
 		{
 			in: inputForPrimer{
-				seq:        "ATGCCGTCGCATTGTCCATCT",
-				restrict:   "GAATTC",
-				seqStart:   10,
-				length:     16,
-				random:     4,
-				startCodon: true,
+				seq:      "ATGCCGTCGCATTGTCCATCT",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   4,
+				addCodon: true,
 			},
 			want: "",
 			err:  errors.New("invalid input, the given sequence (21 nucleotides) is not long enough for a primer of length = 16 starting at nucleotide 10 (25 > 21)"),
@@ -77,24 +77,24 @@ func TestFindForward(t *testing.T) {
 		// test valid input for `random'
 		{
 			in: inputForPrimer{
-				seq:        "ATGCCGTCGCATTGTCCATCT",
-				restrict:   "GAATTC",
-				seqStart:   10,
-				length:     16,
-				random:     1,
-				startCodon: true,
+				seq:      "ATGCCGTCGCATTGTCCATCT",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   1,
+				addCodon: true,
 			},
 			want: "",
 			err:  errors.New("invalid input random = 1, expected integer value between 2 and 10"),
 		},
 		{
 			in: inputForPrimer{
-				seq:        "ATGCCGTCGCATTGTCCATCT",
-				restrict:   "GAATTC",
-				seqStart:   10,
-				length:     16,
-				random:     22,
-				startCodon: true,
+				seq:      "ATGCCGTCGCATTGTCCATCT",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   22,
+				addCodon: true,
 			},
 			want: "",
 			err:  errors.New("invalid input random = 22, expected integer value between 2 and 10"),
@@ -102,12 +102,12 @@ func TestFindForward(t *testing.T) {
 		// sequence does not start with an 'ATG' (start codon) and `startCodon' is false
 		{
 			in: inputForPrimer{
-				seq:        "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
-				restrict:   "GAATTC",
-				seqStart:   10,
-				length:     16,
-				random:     8,
-				startCodon: false,
+				seq:      "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   8,
+				addCodon: false,
 			},
 			want: "",
 			err:  errors.New("input sequence does not begin with a start codon ('ATG')\nmake sure to automatically add a start codon by setting `startCodon' to `true'"),
@@ -115,12 +115,12 @@ func TestFindForward(t *testing.T) {
 		// addition of 'ATG' start codon
 		{
 			in: inputForPrimer{
-				seq:        "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
-				restrict:   "GAATTC",
-				seqStart:   8,
-				length:     16,
-				random:     3,
-				startCodon: true,
+				seq:      "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
+				restrict: "GAATTC",
+				seqStart: 8,
+				length:   16,
+				random:   3,
+				addCodon: true,
 			},
 			want: "GCTGAATTCATGCGCATTGTCCATCTTA", /* start codon should be inserted right after recognition sequence */
 			err:  nil,
@@ -128,12 +128,12 @@ func TestFindForward(t *testing.T) {
 		// invalid `seqStart'
 		{
 			in: inputForPrimer{
-				seq:        "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
-				restrict:   "GAATTC",
-				seqStart:   0,
-				length:     16,
-				random:     3,
-				startCodon: true,
+				seq:      "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
+				restrict: "GAATTC",
+				seqStart: 0,
+				length:   16,
+				random:   3,
+				addCodon: true,
 			},
 			want: "",
 			err:  errors.New("invalid input: primer start point must be an integer > 0 (not 0)"),
@@ -142,16 +142,16 @@ func TestFindForward(t *testing.T) {
 
 	// loop over test cases
 	for _, c := range cases {
-		got, err := FindForward(c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.startCodon)
+		got, err := FindForward(c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon)
 
 		// test similarity of expected and received value
 		if got != c.want {
-			t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.startCodon, got, c.want)
+			t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, got, c.want)
 		}
 
 		// if no error is returned, test if none is expected
 		if err == nil && c.err != nil {
-			t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.startCodon, err, c.err)
+			t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, err, c.err)
 		}
 
 		// if error is returned, test if an error is expected
@@ -160,10 +160,160 @@ func TestFindForward(t *testing.T) {
 			// else if an error is wanted and received but error messages are not the same
 			// print wanted and received error
 			if c.err == nil {
-				t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.startCodon, err, c.err)
+				t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, err, c.err)
 			} else if err.Error() != c.err.Error() {
-				t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.startCodon, err, c.err)
+				t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, err, c.err)
 			}
 		}
 	}
 }
+
+func TestFindReverse(t *testing.T) {
+	cases := []testCasePrimer{
+		// test invalid input
+		// test `seq' with non-nucleotide letters
+		{
+			in: inputForPrimer{
+				seq:      "QVDASTGASD", /* first invalid letter should result in an error */
+				restrict: "GAATTC",
+				seqStart: 3,
+				length:   3,
+				random:   4,
+				addCodon: true,
+			},
+			want: "",
+			err:  errors.New("invalid input Q at position 1, expected sequence of lower or upper case A,T,C,G"),
+		},
+		{
+			in: inputForPrimer{
+				seq:      "ATGCCGVDASTGASD", /* first invalid letter should result in an error */
+				restrict: "GAATTC",
+				seqStart: 3,
+				length:   3,
+				random:   4,
+				addCodon: true,
+			},
+			want: "",
+			err:  errors.New("invalid input V at position 7, expected sequence of lower or upper case A,T,C,G"),
+		},
+		// test `seq' that is exactly of length (`length' + `seqStart' - 1)
+		{
+			in: inputForPrimer{
+				seq:      "ATGCCGTCGCATTCTG",
+				restrict: "GAATTC",
+				seqStart: 1,
+				length:   16,
+				random:   4,
+				addCodon: true,
+			},
+			want: "AGCTGAATTCATGCCGTCGCATTCTG", /* the expected output is the entire sequence plus overhang */
+			err:  nil,
+		},
+		// test `seq' that is shorter than (`length' + `seqStart' - 1)
+		{
+			in: inputForPrimer{
+				seq:      "ATGCCGTCGCATTGTCCATCT",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   4,
+				addCodon: true,
+			},
+			want: "",
+			err:  errors.New("invalid input, the given sequence (21 nucleotides) is not long enough for a primer of length = 16 starting at nucleotide 10 (25 > 21)"),
+		},
+		// test valid input for `random'
+		{
+			in: inputForPrimer{
+				seq:      "ATGCCGTCGCATTGTCCATCT",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   1,
+				addCodon: true,
+			},
+			want: "",
+			err:  errors.New("invalid input random = 1, expected integer value between 2 and 10"),
+		},
+		{
+			in: inputForPrimer{
+				seq:      "ATGCCGTCGCATTGTCCATCT",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   22,
+				addCodon: true,
+			},
+			want: "",
+			err:  errors.New("invalid input random = 22, expected integer value between 2 and 10"),
+		},
+		// sequence does not start with an 'ATG' (start codon) and `startCodon' is false
+		{
+			in: inputForPrimer{
+				seq:      "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
+				restrict: "GAATTC",
+				seqStart: 10,
+				length:   16,
+				random:   8,
+				addCodon: false,
+			},
+			want: "",
+			err:  errors.New("input sequence does not begin with a start codon ('ATG')\nmake sure to automatically add a start codon by setting `startCodon' to `true'"),
+		},
+		// addition of 'ATG' start codon
+		{
+			in: inputForPrimer{
+				seq:      "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
+				restrict: "GAATTC",
+				seqStart: 8,
+				length:   16,
+				random:   3,
+				addCodon: true,
+			},
+			want: "GCTGAATTCATGCGCATTGTCCATCTTA", /* start codon should be inserted right after recognition sequence */
+			err:  nil,
+		},
+		// invalid `seqStart'
+		{
+			in: inputForPrimer{
+				seq:      "CTGCCGTCGCATTGTCCATCTTACTGACCTGATGTGCCA",
+				restrict: "GAATTC",
+				seqStart: 0,
+				length:   16,
+				random:   3,
+				addCodon: true,
+			},
+			want: "",
+			err:  errors.New("invalid input: primer start point must be an integer > 0 (not 0)"),
+		},
+	}
+
+	// loop over test cases
+	for _, c := range cases {
+		got, err := FindForward(c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon)
+
+		// test similarity of expected and received value
+		if got != c.want {
+			t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, got, c.want)
+		}
+
+		// if no error is returned, test if none is expected
+		if err == nil && c.err != nil {
+			t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, err, c.err)
+		}
+
+		// if error is returned, test if an error is expected
+		if err != nil {
+			// if c.err is nil, print wanted and received error
+			// else if an error is wanted and received but error messages are not the same
+			// print wanted and received error
+			if c.err == nil {
+				t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, err, c.err)
+			} else if err.Error() != c.err.Error() {
+				t.Errorf("FindForward(%v, %v, %v, %v, %v, %v) == %v, want %v\n", c.in.seq, c.in.restrict, c.in.seqStart, c.in.length, c.in.random, c.in.addCodon, err, c.err)
+			}
+		}
+	}
+}
+
+// with stop codon: ATGCAAAAACGGGCGATTTATCCGGGTACTTTCGATCCCATTACCAATGGTCATATCGATATCGTGACGCGCGCCACGCAGATGTTCGATCACGTTATTCTGGCGATTGCCGCCAGCCCCAGTAAAAAACCGATGTTTACCCTGGAAGAGCGTGTGGCACTGGCACAGCAGGCAACCGCGCATCTGGGGAACGTGGAAGTGGTCGGGTTTAGTGATTTAATGGCGAACTTCGCCCGTAATCAACACGCTACGGTGCTGATTCGTGGCCTGCGTGCGGTGGCAGATTTTGAATATGAAATGCAGCTGGCGCATATGAATCGCCACTTAATGCCGGAACTGGAAAGTGTGTTTCTGATGCCGTCGAAAGAGTGGTCGTTTATCTCTTCATCGTTGGTGAAAGAGGTGGCGCGCCATCAGGGCGATGTCACCCATTTCCTGCCGGAGAATGTCCATCAGGCGCTGATGGCGAAGTTAGCGTAG
