9d446e22f638d11b8fb3cacdd88dbd70d5965a74
Daniel Schuette <d.schuette@online.de>
2018-08-15 01:03:23 -0400

    add --help information to binary

--- cmd/goprimer.go
+++ cmd/goprimer.go
@@ -10,12 +10,13 @@ import (
 )
 
 var (
-	seqFile     = flag.String("seq_file", "", "valid file path to a ")
-	enzymeFile  = flag.String("enzyme_file", "", "valid file path to a *.re file with correctly formatted restriction enzyme information\ndefault is the file at 'github.com/DanielSchuette/assets/enzymes.re'")
+	seqFile     = flag.String("seq_file", "assets/tp53.seq", "valid file path to a *.seq file with correctly formatted DNA sequence information\ndefault is the file at 'github.com/DanielSchuette/assets/tp53.seq'")
+	enzymeFile  = flag.String("enzyme_file", "assets/enzymes.re", "valid file path to a *.re file with correctly formatted restriction enzyme information\ndefault is the file at 'github.com/DanielSchuette/assets/enzymes.re'")
 	enzymeNameF = flag.String("enzyme_name_forward", "", "name of the enzyme you want to use for the 5' end (must be in the '--enzyme_file')")
 	enzymeNameR = flag.String("enzyme_name_reverse", "", "name of the enzyme you want to use for the 3' end (must be in the '--enzyme_file')")
 	overhangF   = flag.Int("overhang_forward", 4, "number of random nucleotides added to the forward primer (an integer between 2 - 10)")
 	overhangR   = flag.Int("overhang_reverse", 4, "number of random nucleotides added to the reverse primer (an integer between 2 - 10)")
+	verbose     = flag.Bool("verbose", false, "enable verbose output (defaults to false)")
 )
 
 func main() {
