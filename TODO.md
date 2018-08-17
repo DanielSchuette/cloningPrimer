1965c6b7c5021958974ff7d8b3b9a924b2c2c851
Daniel Schuette <d.schuette@online.de>
2018-08-17 19:35:29 -0400

    add TODO.md

---
+++ TODO.md
@@ -0,0 +1,38 @@
+# Main TODO's
+1. Methylation can block cleavage by some restriction enzymes. In E. coli, Dam methylase affects the sequence GATC and Dcm methylase affects the sequence CCAGG or CCTGG. If these sequences are present, you will need to use a dam-, dcm- E. coli strain to grow your plasmid.
+
+2. check 3' end of primer (3 or more G or C bases at this position - may stabilize nonspecific annealing; 3' thymidine - more prone to mispriming)
+
+3. primer length (18 - 30 nucleotides)
+
+4. self-complementarity (primer dimers)
+
+5. GC content (40 - 60%)
+
+6. annealing temperature between overlap of target and primer (delta 2 - 4°C and > 60°C, according to: Tm = 2°C * (A + T) + 4°C * (C + G))
+
+7. N-terminal ATG in-frame with ORF
+
+8. C-terminal TAA (preferred because less prone to read-through than TAG and TGA; 2 or 3 stop codons are possible) in-frame with ORF
+
+
+## More TODO's
+1. add more unit tests
+
+2. set up Travis CI
+
+2. Web interface
+
+3. CLI
+
+4. API documentation
+
+5. calculate stats (see above): func CalcStats()
+
+6. check sequence and recognition sites for restriction sites, start codons, stop codon, ...
+
+7. pull plasmid information from file and test for restriction sites: func CheckPlasmid ()
+
+8. offer access to NCBI blast API (if available?): Blast()
+
+9. check spaces in sequence and silently delete them
--- doc.go
+++ doc.go
@@ -1,44 +1,5 @@
 // Package cloningprimer ...
 //
-//
-// TODO: Methylation can block cleavage by some restriction enzymes. In E. coli, Dam methylase affects the sequence GATC and Dcm methylase affects the sequence CCAGG or CCTGG. If these sequences are present, you will need to use a dam-, dcm- E. coli strain to grow your plasmid.
-//
-// TODO: check 3' end of primer (3 or more G or C bases at this position - may stabilize nonspecific annealing; 3' thymidine - more prone to mispriming)
-//
-// TODO: primer length (18 - 30 nucleotides)
-//
-// TODO: self-complementarity (primer dimers)
-//
-// TODO: GC content (40 - 60%)
-//
-// TODO: annealing temperature between overlap of target and primer (delta 2 - 4°C and > 60°C, according to: Tm = 2°C * (A + T) + 4°C * (C + G))
-//
-// TODO: N-terminal ATG in-frame with ORF
-//
-// TODO: C-terminal TAA (preferred because less prone to read-through than TAG and TGA; 2 or 3 stop codons are possible) in-frame with ORF
-//
-//
-// More TODO's:
-//
-// - add more unit tests
-//
-// - Web interface
-//
-// - CLI
-//
-// - API documentation
-//
-// - calculate stats (see above): func CalcStats()
-//
-// - check sequence and recognition sites for restriction sites, start codons, stop codon, ...
-//
-// - pull plasmid information from file and test for restriction sites: func CheckPlasmid ()
-//
-// - offer access to NCBI blast API (if available?): Blast()
-//
-// - check spaces in sequence and silently delete them
-//
-//
 // Code Example:
 //
 /*
